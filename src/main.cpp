828d989711cdae8412fdfcae68cf7e6076520224
Red Williams <red@gencitylabs.io>
2024-07-10 11:26:05 -0700

    add button interaction with builtinLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <Arduino.h>
+#define pin_button 5
 
+int inPin = 5;
+int ledPin = 13;
+int val = 0;
 // put function declarations here:
 int myFunction(int, int);
 String getUserInput();
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+
+  // LED_BUILTIN = pin 13, built in LED
+  pinMode(LED_BUILTIN, OUTPUT);
+
+// 
+  pinMode(pin_button, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
@@ -17,22 +26,28 @@ void loop() {
   // digitalWrite(13, LOW);
   // delay(1000);
 
+val = digitalRead(inPin);
+digitalWrite(ledPin, val);
 
-  Serial.println("Enter data:");
 
-  while (Serial.available() == 0) {}     //wait for data available
+  
 
-String teststr = getUserInput();
 
-  if (teststr == "red" || teststr == "blue" || teststr == "yellow") {
+//   Serial.println("Enter data:");
 
-    Serial.println("A primary color");
+//   while (Serial.available() == 0) {}     //wait for data available
 
-  } else {
+// String teststr = getUserInput();
 
-    Serial.println("Something else");
+//   if (teststr == "red" || teststr == "blue" || teststr == "yellow") {
 
-  }
+//     Serial.println("A primary color");
+
+//   } else {
+
+//     Serial.println("Something else");
+
+//   }
 
     
   // Serial.print("Feelin Lucky");
